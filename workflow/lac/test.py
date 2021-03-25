05368a012f2f761954f55ea39f3731dfa9b216b4
mpiress <michel@cefetmg.br>
2021-03-24 22:29:40 -0300

    Update NNS

--- workflow/lac/test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from workflow.lac.scripts.config import config
-from multiprocessing import Pool
-
-if __name__ == "__main__":
-    line = "python3 worker.py & "
-    for _ in range(config.NWORKERS-1):
-        line += line
-    subprocess.run(line, shell=True)
