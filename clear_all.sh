8cdde1406c2ba0f7cc3cf52867c1b4d6fc42128f
mpiress <michel@cefetmg.br>
2022-04-24 19:16:45 -0300

    update nscale image path

--- clear_all.sh
+++ clear_all.sh
@@ -1,4 +1,4 @@
 # remove __pycache__ into the project folders 
 
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-find . | grep -E "(__pycache__(2)|\.pyc|\.pyo$)" | xargs rm -rf
+find . | grep -E "('__pycache__ (2)'|\.pyc|\.pyo$)" | xargs rm -rf
